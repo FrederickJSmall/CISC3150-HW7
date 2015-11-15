ab4ecfa0246d0d19823a3fc8a99ed7322acedb50
Fred Small <Fsmall@startoffsmall.com>
2015-11-15 18:11:25 -0500

    add filewriter to process

--- src/dirtree/DirTree.java
+++ src/dirtree/DirTree.java
@@ -1,10 +1,23 @@
 package dirtree;
 
+import java.io.IOException;
+
 public class DirTree {
 	public static void main (String[] args)
 	{
 		TreeDiagram treeDiagram = new TreeDiagram();
-		treeDiagram.tree("D:\\Projects",0);
-		treeDiagram = null;
+		try
+		{
+		    //treeDiagram.tree("D:\\Projects",0);
+			treeDiagram.Open();
+			treeDiagram.tree("/Projects",0);
+			treeDiagram.Close();
+			treeDiagram = null;
+		}
+		catch (IOException ex)
+		{
+			System.out.println("Error encouneted in process");
+			System.out.println(ex.getStackTrace());
+		}
 	}
 }
--- src/dirtree/TreeDiagram.java
+++ src/dirtree/TreeDiagram.java
@@ -1,10 +1,30 @@
 package dirtree;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TreeDiagram {
-
-	public static void tree(String rootDirectory,int level)
+	
+	FileWriter writer;
+	
+	public void Open() throws IOException
+	{
+		writer = new FileWriter("dir_tree.txt");		
+	}
+	public void Close() throws IOException
+	{
+		writer.flush();
+		writer.close();
+	}
+	public void tree(String rootDirectory,int level) throws IOException
+	{
+		String message = String.format("%s\n",rootDirectory);
+		System.out.printf(message);
+		writer.write(message);
+		displaySubTree(rootDirectory,level);
+	}
+	public void displaySubTree(String rootDirectory,int level) throws IOException
 	{
 			level++;
 			File file = new File(rootDirectory);
@@ -15,19 +35,43 @@ public class TreeDiagram {
 			
 			for(String path : fileList )
 			{
-				String fullPath = rootDirectory + "\\" + path;
+				String fullPath = rootDirectory + "/" + path;
 				File checkfile = new File(fullPath);
 				if (checkfile.isDirectory() == true)
 				{
-					System.out.printf("%010d ",level);
+					String message = String.format("%010d|",level);
+					System.out.printf(message);
+					writer.write(message);
 					for (int i=1;i<level;i++)
-						System.out.print("   ");
+					{
+						message = "   ";
+						System.out.print(message);
+						writer.write(message);
+					}
 					
-					System.out.print("|---");
+					if (level==1)
+					{
+						message = "----";
+						System.out.print(message);
+						writer.write(message);
+					}
+					else
+					{
+						message = "|---";
+						System.out.print(message);
+						writer.write(message);
+					}
+					
 					System.out.println(path);
-					tree(fullPath,level);
+					writer.write(path);
+
+					displaySubTree(fullPath,level);
 				}
 			}
 	}
+	//private void output(String message) throws IOException
+	//{
+	//	writer.write(message);
+	//}
 	
 }
