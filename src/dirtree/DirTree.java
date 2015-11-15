867a19889dd984367a5ecaccb3134b989769e0eb
Fred Small <Fsmall@startoffsmall.com>
2015-11-15 17:03:07 -0500

    initial tree results

--- src/dirtree/DirTree.java
+++ src/dirtree/DirTree.java
@@ -4,7 +4,7 @@ public class DirTree {
 	public static void main (String[] args)
 	{
 		TreeDiagram treeDiagram = new TreeDiagram();
-		
-		
+		treeDiagram.tree("D:\\Projects",0);
+		treeDiagram = null;
 	}
 }
--- src/dirtree/TreeDiagram.java
+++ src/dirtree/TreeDiagram.java
@@ -4,9 +4,29 @@ import java.io.File;
 
 public class TreeDiagram {
 
-	public static void tree(String rootDirectory)
+	public static void tree(String rootDirectory,int level)
 	{
-
+			level++;
+			File file = new File(rootDirectory);
+			
+			String[] fileList = file.list();
+			
+			if (fileList == null) return;
+			
+			for(String path : fileList )
+			{
+				String fullPath = rootDirectory + "\\" + path;
+				File checkfile = new File(fullPath);
+				if (checkfile.isDirectory() == true)
+				{
+					System.out.printf("%10d",level);
+					for (int i=0;i<level;i++)
+						System.out.print("-");
+					System.out.print("|");
+					System.out.println(path);
+					tree(fullPath,level);
+				}
+			}
 	}
 	
 }
